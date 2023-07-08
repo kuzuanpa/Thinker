a59fc578510f7a639af6f747644f3a4e990783c8
kuzuanpa <misaka16384@outlook.com>
2023-07-08 08:25:44 +0800

    init

---
+++ src/main/java/cn/kuzuanpa/thinker/Thinker.java
@@ -0,0 +1,19 @@
+package cn.kuzuanpa.thinker;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Thinker.MOD_ID, version = Thinker.VERSION)
+public class Thinker
+{
+    public static final String MOD_ID = "thinker";
+    public static final String VERSION = "0.0.1";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        //
+    }
+}
