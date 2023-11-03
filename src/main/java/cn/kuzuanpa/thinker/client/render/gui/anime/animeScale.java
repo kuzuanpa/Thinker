bbb218f1c62b06140ae96be777b5b7e02e6c5451
kuzuanpa <misaka16384@outlook.com>
2023-11-03 19:56:08 +0800

    scale anime

--- src/main/java/cn/kuzuanpa/thinker/client/render/gui/anime/animeScale.java
+++ src/main/java/cn/kuzuanpa/thinker/client/render/gui/anime/animeScale.java
@@ -1,9 +1,23 @@
 package cn.kuzuanpa.thinker.client.render.gui.anime;
 
+import org.lwjgl.opengl.GL11;
+
 public class animeScale implements IAnime{
+    public animeScale(int startTime,int endTime,float scaleRate,float scaleX,float scaleY){
+        this.startTime=startTime;
+        this.endTime=endTime;
+        this.scaleRate=scaleRate;
+        this.scaleX=scaleX;
+        this.scaleY=scaleY;
+    }
+    public int startTime, endTime;
+    public float scaleRate,scaleX,scaleY;
     @Override
     public void animeDraw(long initTime, long currentTime) {
-
+        long timer = currentTime-initTime;
+        if(timer<startTime) return;
+        if(timer<endTime) GL11.glScalef(((float)(timer - startTime)/(float)(endTime-startTime))*scaleX*scaleRate,((float)(timer - startTime)/(float)(endTime-startTime))*scaleY*scaleRate,1);
+        else GL11.glScalef(scaleX*scaleRate,scaleY*scaleRate,1);
     }
 
     @Override
